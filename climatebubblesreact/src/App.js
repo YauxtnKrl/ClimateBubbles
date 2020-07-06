3cfea28b3d8ad2f593481a3b0a328dc9f044f59f
Justin <justinccarroll1@gmail.com>
2020-07-06 13:53:57 -0400

    data manipulation+results management

---
+++ climatebubblesreact/src/App.js
@@ -0,0 +1,68 @@
+  //Justin Carroll
+import React, {useState} from 'react';
+import './App.css';
+
+
+function App() {
+
+const BASE_URL = "https://developer.nrel.gov/api/cleap/v1/"
+const TYPE = "/state_co2_emissions?state_abbr=CA&type=commercial&"
+const APIKEY = "api_key=Ow65NJHAt85c1BuNJfxJkm8v3egFLusvPPgZ9DV2"
+
+let [apiResponse, setApiResponse] = useState(false)
+
+let climateData= ""
+
+    async function Fetchtime(){
+       climateData = await(await fetch(BASE_URL + TYPE + APIKEY).catch(handleErr)).json();
+      
+       if(climateData){setApiResponse(true)}
+
+      dataList()
+
+      console.log(climateData)
+      return 
+    }
+
+    function handleErr(err) {
+      setApiResponse(false)
+      console.warn(err);
+      let resp = new Response(
+        JSON.stringify({
+          code: 400,
+          message: "Network Error"
+        })
+      );
+      return resp;
+    }
+
+    async function dataList(){
+      await (climateData)
+      let sourceList = Object.entries(climateData.result[0].data)
+    
+      if(apiResponse){
+       sourceList.map((entry)=>{
+         console.log(entry)
+         return(
+           <li>{entry[0]} : {entry[1]}</li>
+         )
+       })
+      }
+      if(!apiResponse){
+        console.log("no data")
+        return(
+          <p>No data</p>
+        )
+      }
+    }
+
+  return (
+    <div className="App" onLoad={Fetchtime}>
+     <p>Hello</p>
+     {dataList()}
+     <button onClick={Fetchtime}>Load Data</button>
+    </div>
+  );
+}
+
+export default App;
